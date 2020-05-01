e470d354d60dbe19965a5071210e1aca7c15f7a8
”nezu” <tonight.tonight.018@gmail.com>
2020-05-01 15:38:13 +0000

    test02 commit

---
+++ practice02.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = "Nezu";
+if ($name == "Nezu") {
+ echo "私は Nezu です";
+}else {
+ echo " Nezu ではありません";
+}
+
+
+$totale = 0;
+echo $totale;
+
+for ($i = 0; $i <= 10000; $i++) {
+    $totale += $i;
+}
+echo $totale;
+
+
+$fruits = array("apple", "orange", "lemon","coconut","strawberry");
+for ($i = 0; $i < count($fruits); $i++) {
+    echo $fruits[$i];
+    echo "\n";
+}
+
+$strat = 1;
+$end   = 100;
+
+for ($i = $strat; $i <= $end; $i++) {
+    if ($i % 5 == 0) {
+        echo $i;
+    }
+}
